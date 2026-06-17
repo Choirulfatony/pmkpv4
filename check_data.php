a7d95b921b43e160a5a1259b3f796456d0fa80c6
Choirul Fatoni <choirulfatoni@gmail.com>
2026-06-17 14:31:58 +0700

    chore: hapus file debugging

--- check_data.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=siimut', 'root', '');
-$stmt = $db->query('SELECT id, file_name, file_type, file_size, is_folder FROM siimut_file_manager ORDER BY id');
-$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($items, JSON_PRETTY_PRINT);
