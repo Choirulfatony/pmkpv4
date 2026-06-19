72ce42a6c6ddc185ad49ef107dcff92e7507cc78
Choirul Fatoni <choirulfatoni@gmail.com>
2026-06-19 14:43:22 +0700

    Revert "fix(validation): ambil data status A bukan D untuk form validasi"
    This reverts commit 985de49d6f29455fe69f1ce47e52f171ca5271fb.

--- app/Models/IndicatorValidationModel.php
+++ app/Models/IndicatorValidationModel.php
@@ -75,7 +75,7 @@ class IndicatorValidationModel extends Model
               AND result_department_id = ?
               AND YEAR(result_period) = ?
               AND MONTH(result_period) = ?
-              AND result_record_status = 'A'
+              AND result_record_status = 'D'
             ORDER BY result_period ASC
         ", [$indicatorId, $departmentId, $tahun, $bulanStr])->getResult();
     }
